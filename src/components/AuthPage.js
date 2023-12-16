c5afd4c36bcc0bd79631f43f3fc2f1f731ae9c5e
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-12-16 22:54:06 +0200

    update

--- src/components/AuthPage.js
+++ src/components/AuthPage.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import LoginForm from '../LoginForm';
 import '../App.css';
@@ -6,7 +7,7 @@ import './AuthPage.css';
 function AuthPage() {
     const [isLogin, setIsLogin] = useState(true);
     const [formData, setFormData] = useState({ username: '', password: '' });
-    const [registrationError, setRegistrationError] = useState(''); // Define the registration error state
+    const [registrationError, setRegistrationError] = useState('');
 
     const handleChange = (e) => {
         setFormData({
@@ -49,137 +50,57 @@ function AuthPage() {
 
     return (
         <div className="auth-page-container">
-            <h1>Welcome to Tama Insights: Your Portal to Cultural Discovery</h1>
-            {registrationError && <div className="error-message">{registrationError}</div>}
-
-            {isLogin ? (
-                <LoginForm
-                    handleChange={handleChange}
-                    handleSubmit={handleSubmit}
-                    formData={formData}
-                />
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label>Username:</label>
-                        <input
-                            type="text"
-                            name="username"
-                            value={formData.username}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label>Password:</label>
-                        <input
-                            type="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <button type="submit">Register</button>
-                </form>
-            )}
-            <button onClick={() => setIsLogin(!isLogin)}>
-                {isLogin ? 'Create an account? Register' : 'Already have an account? Login'}
-            </button>
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/CGCOdMM1WPo?autoplay=1&mute=1&loop=1&controls=0"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+            
+            <div>
+                <h1>Welcome to Tama Insights: Your Portal to Cultural Discovery</h1>
+                {registrationError && <div className="error-message">{registrationError}</div>}
+    
+                {isLogin ? (
+                    <LoginForm
+                        handleChange={handleChange}
+                        handleSubmit={handleSubmit}
+                        formData={formData}
+                    />
+                ) : (
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label>Username:</label>
+                            <input
+                                type="text"
+                                name="username"
+                                value={formData.username}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label>Password:</label>
+                            <input
+                                type="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <button type="submit">Register</button>
+                    </form>
+                )}
+                <button onClick={() => setIsLogin(!isLogin)}>
+                    {isLogin ? 'Create an account? Register' : 'Already have an account? Login'}
+                </button>
+            </div>
         </div>
     );
 }
 
 export default AuthPage;
-
-
-
-
-// import React, { useState } from 'react';
-// import LoginForm from '../LoginForm';
-// import '../App.css';
-// import './AuthPage.css';
-
-// function AuthPage() {
-//     const [isLogin, setIsLogin] = useState(true);
-//     const [formData, setFormData] = useState({ username: '', password: '' });
-
-//     const handleChange = (e) => {
-//         setFormData({
-//             ...formData,
-//             [e.target.name]: e.target.value
-//         });
-//     };
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         const url = isLogin ? 'https://server-tama.onrender.com/auth/login' : 'https://server-tama.onrender.com/auth/register';
-
-//         try {
-//             const response = await fetch(url, {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                 },
-//                 body: JSON.stringify(formData)
-//             });
-
-//             if (response.ok) {
-//                 const data = await response.json();
-//                 console.log(isLogin ? 'Login successful:' : 'Registration successful:', data);
-
-//             } else {
-//                 console.log(isLogin ? 'Login failed:' : 'Registration failed:', response.status);
-//                 alert('Account created successfully.');
-
-//             }
-//         } catch (error) {
-//             console.error('Request failed:', error);
-
-//         }
-//     };
-//     return (
-//         <div className="auth-page-container">
-//          <h1>Welcome to Tama Insights: Your Portal to Cultural Discovery</h1>
-//     {isLogin ?
-//         <LoginForm
-//             handleChange={handleChange}
-//             handleSubmit={handleSubmit}
-//             formData={formData}
-//         /> :
-//         <form onSubmit={handleSubmit}>
-//             <div>
-//                        <label>Username:</label>
-//                        <input
-//                             type="text"
-//                             name="username"
-//                             value={formData.username}
-//                             onChange={handleChange}
-//                             required
-//                         />
-//                     </div>
-//                     <div>
-//                         <label>Password:</label>
-//                         <input
-//                             type="password"
-//                             name="password"
-//                             value={formData.password}
-//                             onChange={handleChange}
-//                             required
-//                         />
-//                     </div>
-//                     <button type="submit">Register</button>
-//                 </form>
-//     }
-//     <button onClick={() => setIsLogin(!isLogin)}>
-//         {isLogin ? 'Create an account? Register' : 'Already have an account? Login'}
-//     </button>
-// </div>
-
-// )}
-
-// export default AuthPage;
-
-
-
 
